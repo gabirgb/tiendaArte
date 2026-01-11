75957928901d55ac37ca1b80cc78ee722a601c0c
Gabriela <gabriela.rgb@gmail.com>
2026-01-11 20:10:59 -0300

    estilos para generar el grid y trabajando la navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,48 @@ import { NavLink } from "react-router"
 
 export default function NavBar() {
     return (
-        <nav>
-            <NavLink to="/">
-                <h1>Logo</h1>
-            </NavLink>
-            <li><NavLink to="/categoria/samsung">Samsung</NavLink></li>
-            <li><NavLink to="/categoria/xiaomi">Xiaomi</NavLink></li>
-            <li><NavLink to="/categoria/motorola">Motorola</NavLink></li>
-            {/* CartWidget */}
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <NavLink class="navbar-brand" to="/">
+                    <img src="../assets/images/logo-tienda.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
+                Tienda de arte
+                </NavLink>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <NavLink class="nav-link active" aria-current="page" to="/">Todos los productos</NavLink>
+                    </li>
+                    {/* <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li> */}
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Categorías
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><NavLink class="dropdown-item" to="/categoria/:collage">Collage</NavLink></li>
+                        <li><NavLink class="dropdown-item" to="/categoria/:grafito">Grafito</NavLink></li>
+                        <li><NavLink class="dropdown-item" to="/categoria/:pintura">Pintura</NavLink></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><NavLink class="dropdown-item" to="/categoria/:prints">Prints</NavLink></li>
+                    </ul>
+                    </li>
+                    {/* <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li> */}
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <Link to="#">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </Link>
+                </form>
+                </div>
+            </div>
         </nav>
 
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-// importo la f para crear el root de react
 import { createRoot } from "react-dom/client"
 import "./index.css"
-// importo el contenido de mi aplicacion desde aca
 import App from "./App.jsx"
 
 // vinculo con html la const
 const rootDiv = document.getElementById("root")
 
 createRoot(rootDiv).render(
-  //con el metodo render le digo a react qué componente debe mostrar. render != compilar
-  <App />, // SIEMPRE hay q cerrar los tags en react, si no tiene tag de cierre se hace autocierre.
+  <App />
 )
-
-/* *
-! No tocar el archivo main.jsx que se arma por defecto, esto es solo para mostrar qué lineas son indispensables.
-
-*/
