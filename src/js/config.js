a328f3ea31dc805395e792e6639561308656c19e
Gabriela <gabriela.rgb@gmail.com>
2026-02-25 15:41:20 -0300

    aplique variables de entorno en vercel y subo config.js con las vars de firestore

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY, //importo las claves desde las variables de entorno de vercel para no expornerlas en github
+    authDomain: "tiendaarte-cf713.firebaseapp.com",
+    projectId: "tiendaarte-cf713",
+    storageBucket: "tiendaarte-cf713.firebasestorage.app",
+    messagingSenderId: "855483804360",
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+export default firebaseConfig
